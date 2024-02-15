425920863b72bd128c60dcc620dbc0dda5ccaa4e
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-02-15 16:30:50 +0000

    fixed bugs

--- CantaFaSiBank.py
+++ CantaFaSiBank.py
@@ -1,66 +1,80 @@
-#Cria a classe pai Pessoa 
-class Pessoa:
-    def __init__(self, nome, idade, nif):
-        self.nome = nome
-        self.idade = idade  
-        self.nif = nif
+from Modelagem import *
 
-    def exibir_informacoes(self):
-        print(f"Nome: {self.nome}, Idade: {self.idade}, NIF: {self.nif}")
+# Função principal
+def main():
+    # Simulação de banco com um cliente
+    cliente = ContaBancaria("User1", 25, "25 8369 147", "log", "123", "Ruax", 850, "1258-5", 0, []) 
 
-    def definir_limite(self, limite):
-        print(f"{self.nome} terá o limite de €{limite}. \n")
-
-    def definir_idade(self):
-        if self.idade >= 18:
-            print(f"{self.nome} tem {self.idade} anos e pode ter uma conta no banco.")
-        else:
-            print(f"{self.nome} tem {self.idade} anos e não pode ter uma conta no banco.")
-
-#Cria a classe filha AberturaConta, que herda as informações da classe pai
-class AberturaConta(Pessoa):
-    def __init__(self, nome, idade, cpf, endereco, conta):
-        super().__init__(nome, idade, cpf)
-        self.endereco = endereco
-        self.conta = conta
-
-    def exibir_informacoes_conta(self):
-        print(f"\nNome: {self.nome}, Idade: {self.idade}, NIF: {self.nif}, Endereço: {self.endereco}, Conta Nº: {self.conta}")
-
-    def realizar_deposito(self, valor):
-        print(f"{self.nome} realizou um deposito de €{valor} na nova conta.")
-
-    def solicitar_cartao_credito(self):
-        print(f"{self.nome} solicitou um cartão de crédito.\n")
+    # Autenticação
+    login_digitado, senha_digitada = cliente.autenticar()
+ 
+    if login_digitado == cliente.login and senha_digitada == cliente.senha:
+        print("Autenticação bem-sucedida!")
+        while True:
+            # Menu de opções
+            print("\nOpções:")
+            print("1. Consultar Saldo")
+            print("2. Depositar")
+            print("3. Sacar")
+            print("4. Exibir Histórico")
+            print("0. Sair")
+ 
+            escolha = input("Escolha uma opção: ")
+ 
+            if escolha == "1":
+                print("Saldo atual: $", cliente.consultar_saldo())
+            elif escolha == "2":
+                valor = float(input("Digite o valor a depositar: $"))
+                cliente.depositar(valor) 
+                print(f"Depósito no valor de {valor} realizado com sucesso!")
+            elif escolha == "3":
+                valor = float(input("Digite o valor a levantar: $"))
+                cliente.sacar(valor)
+            elif escolha == "4":
+                print("\nHistórico de Transações:")
+                cliente.exibir_historico()
+            elif escolha == "0":
+                print(f"Adeus, {cliente.nome}! Até a próxima.")
+                break
+            else:
+                print("Opção inválida. Tente novamente.")
+    else:
+        print("Autenticação falhou. Verifique seu nome e senha.")
+ 
+ 
+if __name__ == "__main__":
+    main()
 
 # Exemplo de cadastro
-pessoa1 = AberturaConta("Gabriela Canta", 21, "222 222 222", "Rua ABC, 123", 1235-9)
-pessoa1.exibir_informacoes()
-pessoa1.definir_limite(300)
-pessoa1.definir_idade()
-pessoa1.realizar_deposito(1000)
-pessoa1.solicitar_cartao_credito()
+#pessoa1 = Modelagem.AberturaConta("Gabriela Canta", 21, "222 222 222", "Rua ABC, 123", 1235-9)
+#pessoa1.AberturaConta.exibir_informacoes_conta()
+# pessoa1.definir_limite(300)
+# pessoa1.definir_idade()
+# pessoa1.realizar_deposito(1000)
+# pessoa1.solicitar_cartao_credito()
 
+"""
 # Instância da classe Pai (Pessoa)
-pessoa2 = Pessoa("Cecília Canta", 20, "333 333 333")
-pessoa2.exibir_informacoes()
-pessoa2.definir_limite(500)
+pessoa2 = Pessoa.exibir_informacoes_Pessoa("Cecília Canta", 20, "333 333 333")
+pessoa2.Pessoa.exibir_informacoes_Pessoa()
+# pessoa2.definir_limite(500)
 
 # Instância da classe Filha (AberturaConta)
 pessoa2.Pessoa = AberturaConta("Cecília Canta", 20, "333 333 333", "Rua XYZ, 456", 1234)
-pessoa2.Pessoa.exibir_informacoes_conta()
+pessoa2.AberturaConta.exibir_informacoes_conta()
 
 # Instância da classe Filha (AberturaConta)
 pessoa3 = AberturaConta("Leandro Canta", 23, "444 444 444", "Rua XYZ, 456", 1456)
-pessoa3.exibir_informacoes_conta()
-pessoa3.realizar_deposito(1500)
-pessoa3.solicitar_cartao_credito()
+pessoa3.AberturaConta.exibir_informacoes_conta()
+# pessoa3.realizar_deposito(1500)
+# pessoa3.solicitar_cartao_credito()
 
 # Alterando valores dos atributos
-pessoa1.nome = "Gabriela I Canta"
-pessoa2.Pessoa.endereco = "Avenida Principal, 789"
+pessoa1.Pessoa.nome = "Gabriela I Cantarini"
+pessoa2.AberturaConta.endereco = "Avenida Principal, 789"
 
 # Exibindo informações atualizadas
 print("\nInformações após alterações:")
-pessoa1.exibir_informacoes()
-pessoa2.Pessoa.exibir_informacoes_conta()
+pessoa1.Pessoa.exibir_informacoes_Pessoa()
+pessoa2.Pessoa.exibir_informacoes_Pessoa()
+"""
---
+++ Modelagem.py
@@ -0,0 +1,77 @@
+#Cria a classe pai Pessoa 
+class Pessoa:
+    def __init__(self, nome, idade, nif):
+        self.nome = nome
+        self.idade = idade
+        self.nif = nif
+
+    def definir_informacoes_Pessoa(self):
+        self.nome = input("Digite o seu nome: ")
+        self.idade = input("Digite a sua idade: ")
+        self.nif = input("Digite o seu nif: ")
+        #return nome, idade, nif
+        
+    def exibir_informacoes_Pessoa(self):
+        print(f"Nome: {self.nome}, Idade: {self.idade}, NIF: {self.nif}")
+
+    def definir_idade(self):
+        if self.idade >= 18:
+            print(f"{self.nome} tem {self.idade} anos e pode ter uma conta no banco.")
+        else:
+            print(f"{self.nome} tem {self.idade} anos e não pode ter uma conta no banco.")
+ 
+ 
+#Cria a classe filha AberturaConta, que herda as informações da classe pai
+class AberturaConta(Pessoa):
+    def __init__(self, nome, idade, nif, login, senha, endereco, ordenado, conta):
+        super().__init__(nome, idade, nif)
+        self.login = login
+        self.senha = senha
+        self.endereco = endereco
+        self.ordenado = ordenado
+        self.conta = conta
+
+    # Função para autenticação de usuários
+    def autenticar(self):
+        login = input("Digite seu login: ")
+        senha = input("Digite sua senha: ")
+        return login, senha
+    
+    #metodo para exibir informações basicas da conta
+    def exibir_informacoes_conta(self):
+        print(f"\nNome: {self.nome}, Idade: {self.idade}, NIF: {self.nif}, Endereço: {self.endereco}, Conta Nº: {self.conta}")
+        
+    def definir_limite(self):        
+        if self.ordenado <= 750:
+            print(f"{self.nome}, recebe o ordenado de {self.ordenado} e não tem direito a limite de crédito. \n")
+        if self.ordenado > 750:
+            print(f"{self.nome}, recebe o ordenado de {self.ordenado} e terá o limite de € 500. \n")
+
+
+# Módulo para gerenciar contas bancárias
+class ContaBancaria(AberturaConta):
+    def __init__(self, nome, idade, nif, login, senha, endereco, ordenado, conta, saldo, historico):
+        super().__init__(nome, idade, nif, login, senha, endereco, ordenado, conta)
+        self.saldo = 0
+        self.historico = []
+ 
+    def depositar(self, valor):
+        self.saldo += valor
+        self.historico.append(f'Depósito: +${valor}')
+ 
+    def sacar(self, valor):
+        if valor <= self.saldo:
+            self.saldo -= valor 
+            self.historico.append(f'Saque: -${valor}')
+        else:
+            print("Saldo insuficiente.")
+ 
+    def consultar_saldo(self):
+        return self.saldo
+ 
+    def exibir_historico(self):
+        for transacao in self.historico:
+            print(transacao)
+
+
+           
