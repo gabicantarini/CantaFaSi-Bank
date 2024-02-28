a511dad8a70a9ea925b984e780eeaeb1ae2ba73e
Ana Carolina Silva <120376821+AvlisCarolina@users.noreply.github.com>
2024-02-28 11:55:28 +0000

    Changed for Pandas
    atualização de ficheiros com uso da biblioteca pandas.

---
+++ LastTry.py
@@ -0,0 +1,80 @@
+import pandas as pd
+from Modelagem import Pessoa, ContaBancaria, ler_dados, escrever_dados, adicionar_nova_conta
+
+
+def main():
+    print("Banco CantaFaSi")
+    print("1. Criar nova conta.")
+    print("2. Entrar.")
+    print("9. Sair.")
+    escolha1 = int(input("Escolha uma opção: "))
+
+    
+
+    if (escolha1 == 1):
+        try:
+            adicionar_nova_conta()
+            main()
+        except ValueError as e: 
+            print(e)
+            main()
+            
+    
+    elif (escolha1 == 2):
+        login_digitado = input("Digite seu login: ")
+        senha_digitada = input("Digite sua senha: ")
+        dados = ler_dados()
+        cliente = []
+
+        for conta in dados:
+            if conta['login'] == login_digitado and conta ['senha'] == senha_digitada:
+                    cliente = ContaBancaria(conta['nome'], conta['idade'], conta['nif'], conta['login'], 
+                                            conta['senha'],conta['endereco'], conta['ordenado'], 
+                                            conta['conta'], float(conta['saldo']))
+                    break
+        if cliente:
+            print("Autenticação bem-sucedida!")
+            while True:
+                print("\nOpções:")
+                print("1. Consultar Saldo")
+                print("2. Depositar")
+                print("3. Levantar")
+                print("4. Atualizar Dados de Conta")
+                print("9. Sair")
+
+                escolha = input("Escolha uma opção: ")
+
+                if escolha == "1":
+                    print("\nSaldo atual: €", cliente.consultar_saldo())
+                elif escolha == "2":
+                    
+                    valor_deposito = float(input("Digite o valor a depositar: €"))
+                    cliente.depositar(valor_deposito)
+                    print("Depósito realizado com sucesso!")
+                    
+                elif escolha == "3":
+                    valor_saque = float(input("Digite o valor a levantar: €"))
+                    cliente.sacar(valor_saque)
+                    print("Levantamento realizado com sucesso!")
+                    
+                elif escolha == "4":
+                    cliente.atualizar_dados()
+                elif escolha == "9":
+                    
+                    print("Obrigado pela sua preferência.")
+                    break
+                else:
+                    print("Opção inválida. Tente novamente.")
+            
+        else:
+            print("Autenticação falhou. Verifique seu nome e senha.")
+
+    elif (escolha1 == 9):
+        print("Obrigado pela sua preferência.")
+        exit()
+
+    else:
+        print("Opção inválida. Tente novamente.")
+        main()
+
+main()
---
+++ Modelagem1.py
@@ -0,0 +1,113 @@
+import pandas as pd
+
+
+def ler_dados():
+        return pd.read_csv('Dados.csv', encoding='utf-8').to_dict(orient='records')
+
+def escrever_dados(dados):
+        df = pd.DataFrame(dados)
+        df.to_csv('Dados.csv', index=False, encoding='utf-8')
+
+class Pessoa:
+    def __init__(self, nome, idade, nif, login, senha, endereco, ordenado, conta):
+        self.nome = nome
+        self.idade = idade
+        self.nif = nif
+        self.login = login
+        self.senha = senha
+        self.endereco = endereco
+        self.ordenado = float(ordenado)
+        self.conta = int(conta)
+
+def adicionar_nova_conta():
+    dados = ler_dados()
+
+    nome = input("Digite seu nome: ")
+    idade = int(input("Digite sua idade: "))
+    nif = input("Digite seu NIF: ")
+    login = input("Digite seu login: ")
+    senha = input("Digite sua senha: ")
+    endereco = input("Digite seu endereço: ")
+    ordenado = float(input("Digite seu ordenado: "))
+    conta = str(len(ler_dados()) + 1)
+    limite = 500
+
+
+    nova_conta = Pessoa(nome, idade, nif, login, senha, endereco, ordenado, conta)
+    
+    
+    dados.append({
+        'nome': nova_conta.nome,
+        'idade': nova_conta.idade,
+        'nif': nova_conta.nif,
+        'login': nova_conta.login,
+        'senha': nova_conta.senha,
+        'endereco': nova_conta.endereco,
+        'ordenado': nova_conta.ordenado,
+        'conta': nova_conta.conta,
+        'saldo': 0,
+        'limite': limite
+    })
+
+    if int(idade) >= 18:
+        print(f"Nova conta adicionada para {nome}. Dados de acesso:\nNumero de conta: {conta}\nLogin: {login}\nSenha: {senha}")
+    else:
+        raise ValueError("Idade abaixo de 18 anos. Não é possível criar uma conta.")
+        
+    if ordenado <= 750:
+        print(f"{nome}, recebe o ordenado de {ordenado} e não tem direito a limite de crédito. \n")
+        limite = 0
+    else:
+        print(f"{nome}, recebe o ordenado de {ordenado} e terá o limite de € 500. \n")
+    
+    escrever_dados(dados)
+    print("Conta criada com sucesso!")
+
+class ContaBancaria(Pessoa):
+    
+    def __init__(self, nome, idade, nif, login, senha, endereco, ordenado, conta, saldo):
+        super().__init__(nome, idade, nif, login, senha, endereco, ordenado, conta)
+        self.saldo = float(saldo)
+        
+
+    def depositar(self, valor): #por corrigir
+        
+
+        self.saldo += valor
+        print(f'Depósito: +€{valor}') 
+             
+
+
+    def sacar(self, valor): #por corrigir
+        if valor <= self.saldo:
+            self.saldo -= valor
+            print(f'Levantamento: -€{valor}')            
+            
+        else:
+            print("Saldo insuficiente.")
+
+    def consultar_saldo(self):
+        return self.saldo
+
+    def atualizar_dados(self):
+        num_conta = int(input("Digite o número da conta que deseja atualizar: "))
+        dados = ler_dados()
+        conta_existente = next((conta for conta in dados if conta['conta'] == num_conta), None)
+
+        if conta_existente:
+            print("\nInformações atuais da conta:")
+            for chave, valor in conta_existente.items():
+                print(f"{chave.capitalize()}: {valor}")
+
+            opcoes_atualizacao = ['nome', 'idade', 'nif', 'login', 'senha', 'endereco', 'ordenado']
+            for opcao in opcoes_atualizacao:
+                novo_valor = input(f"Digite o novo valor para {opcao.capitalize()} (deixe em branco para manter o valor atual): ")
+                if novo_valor:
+                    conta_existente[opcao] = novo_valor
+
+            escrever_dados(dados)
+            print(f"Conta {num_conta} atualizada com sucesso!")
+        else:
+            print(f"Conta {num_conta} não encontrada.")
+
+
